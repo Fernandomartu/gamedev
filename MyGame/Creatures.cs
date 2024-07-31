69de46a85456a0fc16ad2a38caaf6be3a678ff32
Fernando Marturet <fernando.martu@gmail.com>
2024-07-30 20:44:17 -0400

    finally got different bodyparts rotating correctly with angle constraints

--- MyGame/Creatures.cs
+++ MyGame/Creatures.cs
@@ -69,12 +69,15 @@ namespace SimpleGame
         public List<Vector2> Positions { get; private set; }
         public List<int> Radii { get; private set; }
         public List<Texture2D> Textures { get; private set; }
+        public BodyPart PreviousPart { get; set; }
+        public string Name { get; private set; }
 
-        public BodyPart(List<Vector2> positions, List<Texture2D> textures, List<int> radii)
+        public BodyPart(List<Vector2> positions, List<Texture2D> textures, List<int> radii, string name)
         {
             Positions = positions;
             Textures = textures;
             Radii = radii;
+            Name = name;
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -84,59 +87,51 @@ namespace SimpleGame
                 spriteBatch.Draw(Textures[i], Positions[i], null, Color.White, 0f, new Vector2(Radii[i], Radii[i]), 1f, SpriteEffects.None, 0f);
             }
         }
-
-        public void UpdatePositions(float maxAngleChange)
-        {
-            for (int i = 1; i < Positions.Count; i++)
-            {
-                var direction = Positions[i - 1] - Positions[i];
-                float length = direction.Length();
-
-                if (length < float.Epsilon)
-                {
-                    length = 1;
-                }
-
-                direction.Normalize();
-                float angle = (float)Math.Atan2(direction.Y, direction.X);
-
-                if (i > 1)
-                {
-                    float prevAngle = (float)Math.Atan2(Positions[i - 2].Y - Positions[i - 1].Y, Positions[i - 2].X - Positions[i - 1].X);
-                    float angleDiff = MathHelper.WrapAngle(angle - prevAngle);
-
-                    if (Math.Abs(angleDiff) > maxAngleChange)
-                    {
-                        angle = prevAngle + Math.Sign(angleDiff) * maxAngleChange;
-                    }
-                }
-
-                direction = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
-                Positions[i] = Positions[i - 1] - direction * Radii[i];
-            }
-        }
     }
 
-    public class Lizard : Creature
+        public class Lizard : Creature
     {
+        private List<Vector2> allPositions;
+        private List<int> allRadii;
+
         public Lizard(GraphicsDevice graphicsDevice, Vector2 startPosition)
             : base(200f)
         {
+            // Initialize allPositions and allRadii
+            allPositions = new List<Vector2>();
+            allRadii = new List<int>();
+
+            // Head
+            List<Vector2> headPos = new List<Vector2>
+            {
+                startPosition
+            };
+            List<Texture2D> headTextures = new List<Texture2D>
+            {
+                CreateCircleTexture(graphicsDevice, 20, Color.Green)
+            };
+            List<int> headRadii = new List<int> { 20 };
+            var head = new BodyPart(headPos, headTextures, headRadii, "Head");
+            bodyParts.Add(head);
+            allPositions.AddRange(headPos);
+            allRadii.AddRange(headRadii);
+
             // Body
             List<Vector2> bodyPositions = new List<Vector2>
             {
-                startPosition,
                 startPosition - new Vector2(0, 25),
                 startPosition - new Vector2(0, 50)
             };
             List<Texture2D> bodyTextures = new List<Texture2D>
             {
                 CreateCircleTexture(graphicsDevice, 25, Color.Green),
-                CreateCircleTexture(graphicsDevice, 25, Color.Green),
                 CreateCircleTexture(graphicsDevice, 25, Color.Green)
             };
-            List<int> bodyRadii = new List<int> { 25, 25, 25 };
-            bodyParts.Add(new BodyPart(bodyPositions, bodyTextures, bodyRadii));
+            List<int> bodyRadii = new List<int> { 25, 25 };
+            var body = new BodyPart(bodyPositions, bodyTextures, bodyRadii, "Body") { PreviousPart = head };
+            bodyParts.Add(body);
+            allPositions.AddRange(bodyPositions);
+            allRadii.AddRange(bodyRadii);
 
             // Tail
             List<Vector2> tailPositions = new List<Vector2>
@@ -144,19 +139,20 @@ namespace SimpleGame
                 startPosition - new Vector2(0, 75),
                 startPosition - new Vector2(0, 100),
                 startPosition - new Vector2(0, 125),
-                startPosition - new Vector2(0, 150),
-                startPosition - new Vector2(0, 175)
+                startPosition - new Vector2(0, 150)
             };
             List<Texture2D> tailTextures = new List<Texture2D>
             {
                 CreateCircleTexture(graphicsDevice, 20, Color.Green),
                 CreateCircleTexture(graphicsDevice, 18, Color.Green),
                 CreateCircleTexture(graphicsDevice, 16, Color.Green),
-                CreateCircleTexture(graphicsDevice, 14, Color.Green),
-                CreateCircleTexture(graphicsDevice, 12, Color.Green)
+                CreateCircleTexture(graphicsDevice, 14, Color.Green)
             };
-            List<int> tailRadii = new List<int> { 20, 18, 16, 14, 12 };
-            bodyParts.Add(new BodyPart(tailPositions, tailTextures, tailRadii));
+            List<int> tailRadii = new List<int> { 20, 18, 16, 14 };
+            var tail = new BodyPart(tailPositions, tailTextures, tailRadii, "Tail") { PreviousPart = body };
+            bodyParts.Add(tail);
+            allPositions.AddRange(tailPositions);
+            allRadii.AddRange(tailRadii);
         }
 
         public override void Update(GameTime gameTime, Vector2 targetPosition)
@@ -169,59 +165,95 @@ namespace SimpleGame
             Vector2 direction = targetPosition - HeadPosition;
             direction.Normalize();
 
-            bodyParts[0].Positions[0] += direction * CircleSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            allPositions[0] += direction * CircleSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            // Update body positions
-            bodyParts[0].UpdatePositions(MathHelper.ToRadians(60f));
+            // Debug: Print head position to check movement
+            Console.WriteLine($"Head Position: {allPositions[0]}");
 
-            // Update tail positions, starting from the last position of the body
-            Vector2 lastBodySegment = bodyParts[0].Positions[^1];
-            Vector2 secondLastBodySegment = bodyParts[0].Positions[^2];
-            Vector2 bodyDirection = lastBodySegment - secondLastBodySegment;
-            bodyDirection.Normalize();
-            Vector2 tailStartPosition = lastBodySegment + bodyDirection * bodyParts[1].Radii[0];
+            UpdatePositions(MathHelper.ToRadians(70f)); // Adjust the angle as needed
+        }
 
-            bodyParts[1].Positions[0] = tailStartPosition;
-            for (int i = 1; i < bodyParts[1].Positions.Count; i++)
+        private void UpdatePositions(float maxAngleChange)
+        {
+            for (int i = 1; i < allPositions.Count; i++)
             {
-                Vector2 prevPosition = bodyParts[1].Positions[i - 1];
-                Vector2 currentPosition = bodyParts[1].Positions[i];
-                Vector2 tailDirection = prevPosition - currentPosition;
-                float tailLength = tailDirection.Length();
-
-                if (tailLength < float.Epsilon)
+                Vector2 direction;
+                if (i == 1)
                 {
-                    tailLength = 1;
+                    direction = allPositions[0] - allPositions[i];
+                }
+                else
+                {
+                    direction = allPositions[i - 1] - allPositions[i];
                 }
 
-                tailDirection.Normalize();
-                float tailAngle = (float)Math.Atan2(tailDirection.Y, tailDirection.X);
+                float length = direction.Length();
+                if (length < float.Epsilon)
+                {
+                    length = 1;
+                }
+
+                direction.Normalize();
+                float angle = (float)Math.Atan2(direction.Y, direction.X);
 
                 if (i > 1)
                 {
-                    float prevTailAngle = (float)Math.Atan2(bodyParts[1].Positions[i - 2].Y - bodyParts[1].Positions[i - 1].Y, bodyParts[1].Positions[i - 2].X - bodyParts[1].Positions[i - 1].X);
-                    float tailAngleDiff = MathHelper.WrapAngle(tailAngle - prevTailAngle);
+                    float prevAngle = (float)Math.Atan2(allPositions[i - 2].Y - allPositions[i - 1].Y, allPositions[i - 2].X - allPositions[i - 1].X);
+                    float angleDiff = MathHelper.WrapAngle(angle - prevAngle);
 
-                    if (Math.Abs(tailAngleDiff) > MathHelper.ToRadians(60f))
+                    if (Math.Abs(angleDiff) > maxAngleChange)
                     {
-                        tailAngle = prevTailAngle + Math.Sign(tailAngleDiff) * MathHelper.ToRadians(60f);
+                        angle = prevAngle + Math.Sign(angleDiff) * maxAngleChange;
                     }
                 }
 
-                tailDirection = new Vector2((float)Math.Cos(tailAngle), (float)Math.Sin(tailAngle));
-                bodyParts[1].Positions[i] = prevPosition - tailDirection * bodyParts[1].Radii[i];
+                direction = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+                allPositions[i] = allPositions[i - 1] - direction * allRadii[i];
+            }
+
+            // Update bodyParts with new positions
+            int index = 0;
+            foreach (var part in bodyParts)
+            {
+                for (int i = 0; i < part.Positions.Count; i++, index++)
+                {
+                    part.Positions[i] = allPositions[index];
+                }
             }
         }
     }
 
+
     public class Snake : Creature
     {
+        private List<Vector2> allPositions;
+        private List<int> allRadii;
+
         public Snake(GraphicsDevice graphicsDevice, Vector2 startPosition)
             : base(200f)
         {
+            // Initialize allPositions and allRadii
+            allPositions = new List<Vector2>();
+            allRadii = new List<int>();
+
+            // Head
+            List<Vector2> headPos = new List<Vector2>
+            {
+                startPosition
+            };
+            List<Texture2D> headTextures = new List<Texture2D>
+            {
+                CreateCircleTexture(graphicsDevice, 20, Color.Blue)
+            };
+            List<int> headRadii = new List<int> { 20 };
+            var head = new BodyPart(headPos, headTextures, headRadii, "Head");
+            bodyParts.Add(head);
+            allPositions.AddRange(headPos);
+            allRadii.AddRange(headRadii);
+
+            // Body
             List<Vector2> positions = new List<Vector2>
             {
-                startPosition,
                 startPosition - new Vector2(0, 25),
                 startPosition - new Vector2(0, 50),
                 startPosition - new Vector2(0, 75),
@@ -234,11 +266,13 @@ namespace SimpleGame
                 CreateCircleTexture(graphicsDevice, 25, Color.Red),
                 CreateCircleTexture(graphicsDevice, 25, Color.Red),
                 CreateCircleTexture(graphicsDevice, 25, Color.Red),
-                CreateCircleTexture(graphicsDevice, 25, Color.Red),
                 CreateCircleTexture(graphicsDevice, 25, Color.Red)
             };
-            List<int> radii = new List<int> { 25, 25, 25, 25, 25, 25 };
-            bodyParts.Add(new BodyPart(positions, textures, radii));
+            List<int> radii = new List<int> { 25, 25, 25, 25, 25 };
+            var body = new BodyPart(positions, textures, radii, "Body") { PreviousPart = head };
+            bodyParts.Add(body);
+            allPositions.AddRange(positions);
+            allRadii.AddRange(radii);
         }
 
         public override void Update(GameTime gameTime, Vector2 targetPosition)
@@ -251,11 +285,60 @@ namespace SimpleGame
             Vector2 direction = targetPosition - HeadPosition;
             direction.Normalize();
 
-            bodyParts[0].Positions[0] += direction * CircleSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            allPositions[0] += direction * CircleSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+            // Debug: Print head position to check movement
+            Console.WriteLine($"Head Position: {allPositions[0]}");
+
+            UpdatePositions(MathHelper.ToRadians(30f)); // Adjust the angle as needed
+        }
+
+        private void UpdatePositions(float maxAngleChange)
+        {
+            for (int i = 1; i < allPositions.Count; i++)
+            {
+                Vector2 direction;
+                if (i == 1)
+                {
+                    direction = allPositions[0] - allPositions[i];
+                }
+                else
+                {
+                    direction = allPositions[i - 1] - allPositions[i];
+                }
+
+                float length = direction.Length();
+                if (length < float.Epsilon)
+                {
+                    length = 1;
+                }
+
+                direction.Normalize();
+                float angle = (float)Math.Atan2(direction.Y, direction.X);
+
+                if (i > 1)
+                {
+                    float prevAngle = (float)Math.Atan2(allPositions[i - 2].Y - allPositions[i - 1].Y, allPositions[i - 2].X - allPositions[i - 1].X);
+                    float angleDiff = MathHelper.WrapAngle(angle - prevAngle);
+
+                    if (Math.Abs(angleDiff) > maxAngleChange)
+                    {
+                        angle = prevAngle + Math.Sign(angleDiff) * maxAngleChange;
+                    }
+                }
+
+                direction = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+                allPositions[i] = allPositions[i - 1] - direction * allRadii[i];
+            }
+
+            // Update bodyParts with new positions
+            int index = 0;
             foreach (var part in bodyParts)
             {
-                part.UpdatePositions(MathHelper.ToRadians(40f));
+                for (int i = 0; i < part.Positions.Count; i++, index++)
+                {
+                    part.Positions[i] = allPositions[index];
+                }
             }
         }
     }
